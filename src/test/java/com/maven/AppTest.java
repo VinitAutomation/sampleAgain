d8cfc592180c8915941a6481b90d06a3a7328a24
VinitAutomation <kvinit538@gmail.com>
2022-09-22 09:12:42 +0530

    adding files

--- src/test/java/com/maven/AppTest.java
+++ src/test/java/com/maven/AppTest.java
@@ -37,7 +37,7 @@ public class AppTest
 	public void verifyOneStopFlightCount() throws IOException {
 		searchFlightObj.searchFlightsBetweenSourceToDestination(driver, utilityObj.getValueFromProp("url"), utilityObj.getValueFromProp("source"), utilityObj.getValueFromProp("destination"));
 		System.out.println("No. of Flights having one stop--> "+ flObj.getOneStopFlightCount());
-		System.out.println("Minimum fare from source to destination--> " +flObj.getMinimumFare());
+		System.out.println("Minimum fare from source to destination is --> " +flObj.getMinimumFare());
 	}
 
 	@AfterTest
