eafb220ae5293b01e1bf03beab6515286cfe2f4e
VinitAutomation <kvinit538@gmail.com>
2022-10-07 16:27:54 +0530

    added 5

--- src/test/java/com/maven/AppTest.java
+++ src/test/java/com/maven/AppTest.java
@@ -37,7 +37,7 @@ public class AppTest
 	public void verifyOneStopFlightCount() throws IOException {
 		searchFlightObj.searchFlightsBetweenSourceToDestination(driver, utilityObj.getValueFromProp("url"), utilityObj.getValueFromProp("source"), utilityObj.getValueFromProp("destination"));
 		System.out.println("No. of Flights having one stop--> "+ flObj.getOneStopFlightCount());
-		System.out.println("Branch3Minimum fare from source to destination is--> " +flObj.getMinimumFare());
+		System.out.println("Branch5Minimum fare from source to destination is--> " +flObj.getMinimumFare());
 
 
 	}
